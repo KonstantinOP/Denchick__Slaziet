0fbc11cacece29c08cf37fabb6630d0ce78a013f
MagicBook <terexin_00@mail.ru>
2025-08-31 16:45:36 +0300

    Task-2 added dialogue

--- app/src/main/java/com/example/denchickslaziet/SecActivity.kt
+++ app/src/main/java/com/example/denchickslaziet/SecActivity.kt
@@ -5,17 +5,34 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Close
+import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.focusModifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -26,27 +43,88 @@ import androidx.core.view.WindowInsetsCompat
 class SecondActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent{
+        setContent {
             SecondScreen()
+            Column {
+                ShowDialogExample()
+            }
+
         }
-    }
-   @Composable
-   fun SecondScreen() {
-       Column(modifier = Modifier
-           .padding(top = 120.dp)
-           .fillMaxWidth(),
-           horizontalAlignment = Alignment.CenterHorizontally){
-           Text("this second activity",
-               fontSize = 30.sp)
-           Spacer(modifier = Modifier. height(160.dp))
-           Button(onClick = {finish()}){
-               Text("back to firrst activity",
-                   fontSize = 30.sp)}
-       }
     }
     @Preview(showBackground = true, showSystemUi = true)
     @Composable
-    fun FirstScreenPreview(){
+    fun ShowDialogExample(){
+     var showDialog by remember { mutableStateOf(false)}
+        Column(modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally) {
+            Button(onClick = {showDialog = true},
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = Color.Red
+                )){
+                Text("Нажми на меня")
+
+            }
+            if (showDialog){
+                AlertDialog(
+                    onDismissRequest = {showDialog = false},
+                    title = {
+                        Row(
+                            modifier = Modifier,
+                           horizontalArrangement = Arrangement.Absolute.SpaceBetween,
+                            verticalAlignment = Alignment.CenterVertically,) {
+                            Text("У меня к тебе вопрос))", fontSize = 20.sp)
+                            Icon(
+                                imageVector = Icons.Default.Close,
+                                contentDescription = "Закрыть",
+                                modifier = Modifier
+                                    .size(20.dp)
+                                    .clickable { showDialog = false }
+
+                            )
+                        }
+                    },
+                    text = {
+                        Text("Сосал?)))") },
+                    confirmButton = {
+                        Button(onClick = {showDialog = false}) {
+                            Text("Да")
+                        }
+                    }
+                )
+            }
+        }
+    }
+    @Composable
+    fun SecondScreen() {
+        Column(
+            modifier = Modifier
+                .padding(top = 120.dp)
+                .fillMaxWidth(),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(
+                "this second activity",
+                fontSize = 30.sp
+            )
+            Spacer(modifier = Modifier.height(160.dp))
+            Button(onClick = { finish() }) {
+                Text(
+                    "back to firrst activity",
+                    fontSize = 30.sp
+                )
+            }
+        }
+    }
+
+    @Preview(showBackground = true, showSystemUi = true)
+    @Composable
+    fun FirstScreenPreview() {
         SecondScreen()
     }
+}
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+fun ShoowDialogExamplePreview() {
+
 }
